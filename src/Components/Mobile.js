252da6915c1fca5789b23011da2475cae177cefd
Victor Hugo Meurer <meurervictorhugo@gmail.com>
2022-05-09 10:20:16 -0300

    Todos os requisitos obrigatorios componentizados

---
+++ src/Components/Mobile.js
@@ -0,0 +1,11 @@
+import Heart from "./HeartIcon";
+
+export default function Mobile(){
+    return (
+        <ion-icon name="home"></ion-icon>,
+        <ion-icon name="search-outline"></ion-icon>,
+        <ion-icon name="add-circle-outline"></ion-icon>,
+        <Heart />,
+        <ion-icon name="person-outline"></ion-icon>
+    );
+}
